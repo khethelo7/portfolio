dfba1e6f900c9a58cf6f32bb2dc135a04981f69f
Khethelo Nkosi <saneleknkosi@gmail.com>
2024-09-26 12:10:03 +0200

    stack(js): Used functionality for toggle active, added cancel-button in corelation with menu-button

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 
 const menuButton = document.getElementById('menu-button');
 const offScreenMenu = document.querySelector(".off-screen-menu");
+const cancelButton = document.getElementById('cancel-button');
 
 menuButton.addEventListener("click", () => {
-    // hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('show-menu');
+    cancelButton.classList.toggle("active");
+    menuButton.classList.toggle("active");
+})
+
+cancelButton.addEventListener("click", () => {
+    offScreenMenu.classList.toggle("show-menu");
+    menuButton.classList.toggle("active");
+    cancelButton.classList.toggle("active");
 })
