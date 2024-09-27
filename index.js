59bb447adf6b5cc23159a9d71d26f31fd3d4a9de
Khethelo Nkosi <saneleknkosi@gmail.com>
2024-09-27 22:58:38 +0200

    stack(js): Added functionality to be able to auto close menu when a link is clicked

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const menuButton = document.getElementById('menu-button');
 const offScreenMenu = document.querySelector(".off-screen-menu");
 const cancelButton = document.getElementById('cancel-button');
 
+const menuLinks = document.querySelectorAll(".off-screen-menu a");
+
+menuLinks.forEach((link) => {
+    link.addEventListener("click", function() {
+        offScreenMenu.classList.remove("show-menu");
+        cancelButton.classList.toggle("active");
+        menuButton.classList.toggle("active");
+    })
+})
+
 menuButton.addEventListener("click", () => {
     offScreenMenu.classList.toggle('show-menu');
     cancelButton.classList.toggle("active");
